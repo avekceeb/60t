2e9cc8b8590d8aef9f2c6fa9d950fa67511dfb6d
me <me@me>
2017-04-17 00:08:34 +0300

    segfault fixed

--- control/daemon.c
+++ control/daemon.c
@@ -12,38 +12,46 @@
 #include <errno.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-//#include <strings.h>
-//#include <sys/wait.h>
 
 #include "60t.h"
 #include "60t-usb.h"
 
-#ifdef verbose
-#include <syslog.h>
-    #define log(...) syslog(LOG_INFO, __VA_ARGS__)
-    #define debug_printf(...) syslog(LOG_INFO, __VA_ARGS__)
+/* web stuff */
+const char * mimehtml = "text/html";
+const char * mimecss = "text/css";
+const char * mimejs = "application/javascript";
+const char * mimejson = "application/json";
+const char * mimetext = "text/plain"; /* let it be the default */
+
+#define welcome_page "60t.html"
+
+#define server_id "Server: 60t\r\n"
+
+#if openwrtbuild
+    #define wwwdir "/www"
 #else
-    #define log(...) 
-    #define debug_printf(...)
+    #define wwwdir "."
+#endif
+
+unsigned short process_request(int);
+int send_file(int, const char *);
+void send_json(int);
+
+#if demonize
+    #include <syslog.h>
+    #define log(...) syslog(LOG_INFO, __VA_ARGS__)
+    #define fatal(...) while(1){syslog(LOG_ERR, __VA_ARGS__);exit(3);}
+#else
+    #define log(...) printf(__VA_ARGS__)
+    #define fatal(...) while(1){printf(__VA_ARGS__);exit(3);}
 #endif
 
 #ifndef max
 #define max(a,b) ((a)>(b)?(a):(b))
 #endif
 
-#define server_id "Server: 60t\r\n"
-
-#if openwrtbuild
-    #define wwwdir "/www/"
-#else
-    #define wwwdir "./"
-#endif
-
-// todo: on fatal do cleanup
-
-unsigned short process_request(int);
-void fatal(const char *);
-void send_file(int, const char *, const char*);
+int server_sock = -1;
+int client_sock = -1;
 
 int lock_fd;
 libusb_device_handle* dev = 0;
@@ -56,7 +64,54 @@ unsigned char pl1 = 0;
 unsigned char pl2 = 0;
 unsigned char pr1 = 0;
 unsigned char pr2 = 0;
-unsigned char buffer[32];
+// todo : structure
+unsigned char usb_buffer[32];
+
+void compose_command() {
+
+    if (pwm_left < 0) {
+        pwm_left = -pwm_left;
+        pl1 = 0;
+        pl2 = 1;
+    } else if (pwm_left > 0) {
+        pl1 = 1;
+        pl2 = 0;
+    } else {
+        pl1 = 0;
+        pl2 = 0;
+    }
+
+    if (pwm_right < 0) {
+        pwm_right = -pwm_right;
+        pr1 = 0;
+        pr2 = 1;
+    } else if (pwm_right > 0) {
+        pr1 = 1;
+        pr2 = 0;
+    } else {
+        pr1 = 0;
+        pr2 = 0;
+    }
+
+    pwm_left &= 0xFF;
+    pwm_right &= 0xFF;
+
+    if (cmd_get == cmd) {
+        // todo: ???
+        usb_buffer[0] = 0;
+    } else if (cmd_set == cmd) {
+        usb_buffer[0] = 1;
+        usb_buffer[1] = pwm_right;
+        usb_buffer[2] = pr1;
+        usb_buffer[3] = pr2;
+        usb_buffer[4] = 2;
+        usb_buffer[5] = pwm_left;
+        usb_buffer[6] = pl1;
+        usb_buffer[7] = pl2;
+    }
+}
+
+/********* usb ********************/
 
 libusb_device_handle* get_usb_device() { 
     
@@ -65,28 +120,27 @@ libusb_device_handle* get_usb_device() {
 
     res = libusb_init(0);
     if (res != 0) {
-        printf("Error initialising libusb.\n");
+        log("Error initialising libusb.\n");
         return 0;
     }
 
     handle = libusb_open_device_with_vid_pid(0, shared_vid, shared_pid);
     if (!handle) {
-        printf("Unable to open device\n");
+        log("Unable to open device\n");
         return 0;
     }
 
-#if 1
     if (libusb_kernel_driver_active(handle, 0)) {
         res = libusb_detach_kernel_driver(handle, 0);
         if (res != 0) {
+            log("Unable to detach kernel driver\n");
           return 0;
         }
     }
-#endif
 
     res = libusb_claim_interface(handle, 0);
     if (res != 0) {
-        printf("Error claiming interface.\n");
+        log("Error claiming interface.\n");
         libusb_close(handle);
         return 0;
     }
@@ -133,60 +187,50 @@ void reset_usb_device() {
     dev = get_usb_device();
 }
 
-void compose_command() {
-    if (pwm_left < 0) {
-        pwm_left = -pwm_left;
-        pl1 = 0;
-        pl2 = 1;
-    } else if (pwm_left > 0) {
-        pl1 = 1;
-        pl2 = 0;
-    } else {
-        pl1 = 0;
-        pl2 = 0;
+void usb_transaction() {
+    
+    int bytes_read;
+    
+    if (!dev) {
+        return;
     }
 
-    if (pwm_right < 0) {
-        pwm_right = -pwm_right;
-        pr1 = 0;
-        pr2 = 1;
-    } else if (pwm_right > 0) {
-        pr1 = 1;
-        pr2 = 0;
-    } else {
-        pr1 = 0;
-        pr2 = 0;
+    log("===>\n");
+    
+    if (cmd_set == cmd) {
+    
+        bytes_read = usb_send(dev, usb_buffer, 8);
+        log("%d bytes sent\n", bytes_read);
+    
+    } else if (cmd_get == cmd) {
+        
+        bytes_read = usb_receive(dev, usb_buffer, report_size);
+        log("%d bytes got\n", bytes_read);
+        log("buffer: %x %x %x %x\n",
+                usb_buffer[0], usb_buffer[1], usb_buffer[2], usb_buffer[3]);
+    
     }
 
-    pwm_left &= 0xFF;
-    pwm_right &= 0xFF;
-
-    if (cmd_get == cmd) {
-        // todo: ???
-        buffer[0] = 0;
-    } else if (cmd_set == cmd) {
-        buffer[0] = 1;
-        buffer[1] = pwm_right;
-        buffer[2] = pr1;
-        buffer[3] = pr2;
-        buffer[4] = 2;
-        buffer[5] = pwm_left;
-        buffer[6] = pl1;
-        buffer[7] = pl2;
-    }
+    log("===>\n\n");
 }
 
-void usb_transaction() {
-    int bytes_read;
-    if (cmd_set == cmd) {
-        bytes_read = usb_send(dev, buffer, 8);
-        log("%d bytes sent", bytes_read);
-    } else if (cmd_get == cmd) {
-        bytes_read = usb_receive(dev, buffer, report_size);
-        log("%d bytes got\n", bytes_read);
-        log("GOT THIS: %x %x %x %x\n",
-                buffer[0], buffer[1], buffer[2], buffer[3]);
+/*******  web ***************/
+
+const char * suggest_mime_type(const char* hint) {
+    if (!hint) {
+        return 0;
     }
+    int len = strlen(hint);
+    if (len > 5 && !strcmp(hint + len - 5, ".html")) {
+        return mimehtml;
+    }
+    if (len > 4 && !strcmp(hint + len - 4, ".css")) {
+        return mimecss;
+    }
+    if (len > 3 && !strcmp(hint + len - 3, ".js")) {
+        return mimejs;
+    }
+    return 0; // text???
 }
 
 // 0 - no command to execute
@@ -198,54 +242,62 @@ unsigned short process_request(int client) {
     unsigned short command_ready = 0;
     char * lines;
 
+    log("--->\n");
+
     if (read(client, buf, 1024) == -1) {
-        fatal("recive");
+        fatal("fatal: receive\n");
     }
 
+    // GET /blah/blah?x=4&y=-10&z=yes HTTP/1.1
     lines = strtok(buf, "\n\r");
     
     do {
-        // GET /sdfsdf/?sfasfasf=45555 HTTP/1.1
         if (0 == strncasecmp(lines, "GET", 3)) {
             strncpy(request, lines, 128);
             request[127] = '\0';
         }
-        debug_printf("-->%s\n", lines);
         lines = strtok(NULL, "\n\r");
     } while (lines);
 
-    debug_printf("R = %s\n", request);
-
-    if (!request[0]) {
-        goto html;
+    if (!request || !request[0]) {
+        goto welcome;
     }
     
+    log("request: %s\n", request);
+
     // split GET request
     lines = strtok(request, " ");
     if (!lines) {
-        goto html;
+        goto welcome;
     }
     
     // expecting resource line
     lines = strtok(NULL, " ");
     if (!lines) {
-        goto html;
+        goto welcome;
     }
-    
+
+    log("resource requested: %s\n", lines);
+
+#if 1
+    if (!strcmp(lines, "/jsondata")) {
+        send_json(client);
+        goto end;
+    }
+#endif
+
+    if (send_file(client, lines)) {
+        goto end;
+    }
+
     // checking query string
     query_string = strstr(lines, "?");
 
-    if (!query_string) {
-        // check if .js requested:
-        int len = strlen(lines);
-        if (len > 3 && !strcmp(lines + len - 3, ".js")) {
-            debug_printf("javascript: %s\n", lines);
-            goto javascript;
-        }
-        goto html;
+    if (!query_string || 2 > strlen(query_string)) {
+        goto welcome;
     }
 
-    debug_printf("Q = %s\n", &(query_string[1]));
+    log("query: %s\n", &(query_string[1]));
 
     lines = &(query_string[1]);
 
@@ -256,34 +308,34 @@ unsigned short process_request(int client) {
     for (current_qry = lines; ; current_qry = NULL) {
         
         token = strtok_r(current_qry, "&", &saveptr1);
-        if (token == NULL)
+        if (!token)
             break;
 
         current_token = token;
 
         key = strtok_r(current_token, "=", &saveptr2);
         
-        if (key == NULL) {
+        if (!key) {
             continue;
         }
         
         value = strtok_r(NULL, "=", &saveptr2);
         
-        if (key == NULL) {
+        if (!key) {
             continue;
         }
 
-        debug_printf ("k=%s  v=%s\n", key, value);
+        log("parameter: %s = %s\n", key, value);
 
-        if ( 0 == strncmp(key,"cmd",3)) {
+        if (0 == strncmp(key, "cmd", 3)) {
             if (0 == strncmp(value, "get", 3)) {
-                debug_printf(" command = get\n");
+                log("command = get\n");
                 cmd = cmd_get;
             } else if (0 == strncmp(value, "set", 3)) {
-                debug_printf(" command = set\n");
+                log("command = set\n");
                 cmd = cmd_set;
             } else if (0 == strncmp(value, "rst", 3)) {
-                debug_printf(" command = reset\n");
+                log("command = reset\n");
                 cmd = cmd_rst;
             }
         }
@@ -293,51 +345,52 @@ unsigned short process_request(int client) {
         else if (0 == strcmp(key, "l")) {
             pwm_left = atoi(value);
         }
+        // todo: ???
+        command_ready = 1;
     }
 
-//ajax:
-    //todo
-    sprintf(buf, "HTTP/1.0 200 OK\r\n" server_id);
-    send(client, buf, strlen(buf), 0);
-    sprintf(buf, "Content-Type: text/plain\r\n\r\n60t\n");
-    send(client, buf, strlen(buf), 0);
-    command_ready = 1;
-    goto end;
-
-// todo: absolute paths ???
-javascript:
-    send_file(client, wwwdir "60t.js", "Content-type: application/javascript\r\n");
-    goto end;
-
-html:
-    send_file(client, wwwdir "60t.html" , "Content-Type: text/html\r\n");
+welcome:
+    if (!send_file(client, welcome_page)) {
+        // last resort:
+        sprintf(buf, "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\n60t\n");
+        send(client, buf, strlen(buf), 0);
+        goto end;
+    }
 
 end:
     close(client);
+    log("<---\n\n");
     return command_ready;
 }
 
-void fatal(const char *msg) {
-    printf(msg);
-    exit(1);
-}
-
-void send_file(int client, const char *filename, const char* type) {
-    FILE *resource = NULL;
+int send_file(int client, const char *filename) {
+    FILE *resource = 0;
     char buf[1024];
+
+    const char * mime = suggest_mime_type(filename);
+    if (!mime) {
+        return 0;
+    }
+
+    sprintf(buf, wwwdir);
+    // todo: check "/" at the beginning
+    strcat(buf, filename);
+
+    resource = fopen(buf, "r");
+    if (!resource) {
+        log("error fopen\n");
+        return 0;
+    }
+
+    log("file = %s\n", buf);
+    
+    memset(buf, 0, 1024);
 
     sprintf(buf, "HTTP/1.0 200 OK\r\n" server_id);
     send(client, buf, strlen(buf), 0);
-    sprintf(buf, type);
-    send(client, buf, strlen(buf), 0);
-    sprintf(buf, "\r\n");
-    send(client, buf, strlen(buf), 0);
+    sprintf(buf, "Content-type: %s\r\n\r\n", mime);
 
-    resource = fopen(filename, "r");
-    if (resource == NULL) {
-        printf("error fopen\n");
-        return;
-    }
+    send(client, buf, strlen(buf), 0);
 
     fgets(buf, sizeof(buf), resource);
     while (!feof(resource)) {
@@ -346,13 +399,38 @@ void send_file(int client, const char *filename, const char* type) {
     }
 
     fclose(resource);
+
+    return 1;
+}
+
+#define jsoncount 128
+void send_json(int client) {
+
+    int i;
+    int data[jsoncount];
+    char buf[4096];
+
+    for (i=0; i<jsoncount; i++) {
+        data[i] = (i-(jsoncount>>1)) * (i-(jsoncount>>1));
+    }
+    
+    sprintf(buf, "HTTP/1.0 200 OK\r\nContent-Type: %s\r\n\r\n", mimejson);
+    send(client, buf, strlen(buf), 0);
+    
+    sprintf(buf, "[\n{\"id\":-100,\"name\":\"Parabola\",\"data\":[\n");
+    send(client, buf, strlen(buf), 0);
+
+    for (i=0; i<jsoncount; i++) {
+        sprintf(buf, "{\"x\":%d,\"y\":%d}%s\n", i, data[i], i==jsoncount-1 ? "":",");
+        send(client, buf, strlen(buf), 0);
+    }
+    sprintf(buf, "]\n}\n]");
+    send(client, buf, strlen(buf), 0);
 }
 
 void httpd_loop(void) {
 
-    int server_sock = -1;
     u_short port = httpd_port;
-    int client_sock = -1;
     struct sockaddr_in client_name;
     socklen_t client_name_len = sizeof(client_name);
 
@@ -360,7 +438,7 @@ void httpd_loop(void) {
 
     server_sock = socket(PF_INET, SOCK_STREAM, 0);
     if (server_sock == -1)
-        fatal("socket");
+        fatal("ERROR: socket\n");
 
     memset(&name, 0, sizeof(name));
     name.sin_family = AF_INET;
@@ -368,10 +446,10 @@ void httpd_loop(void) {
     name.sin_addr.s_addr = htonl(INADDR_ANY);
     
     if (bind(server_sock, (struct sockaddr *)&name, sizeof(name)) < 0)
-        fatal("bind");
+        fatal("ERROR: bind\n");
 
     if (listen(server_sock, 5) < 0)
-        fatal("listen");
+        fatal("ERROR: listen\n");
     
     while (1) {
         
@@ -381,7 +459,7 @@ void httpd_loop(void) {
                 &client_name_len);
         
         if (client_sock == -1)
-            fatal("accept");
+            fatal("ERROR: accept\n");
 
         if (process_request(client_sock)) {
             if (cmd == cmd_rst) {
@@ -397,6 +475,8 @@ void httpd_loop(void) {
     close(server_sock);
 
 }
+
+/*********** daemon *******************/
 
 void kill_previous_instance () {
     int lock_fd;
@@ -456,12 +536,16 @@ int create_pid_file() {
 }
 
 void signal_handler(int sgn) {
-    //todo: close sockets and usb device
-     if (0 == lockf(lock_fd, F_ULOCK, 0)) {
+    log("got signal %d\n", sgn);
+    close(server_sock);
+    close(client_sock);
+    if (0 == lockf(lock_fd, F_ULOCK, 0)) {
         close(lock_fd);
-     }
-     exit(0);
+    }
+    exit(5);
 }
+
+/************************************/
 
 int main(int argc, char **argv) {
 	
@@ -485,6 +569,7 @@ int main(int argc, char **argv) {
         }
     }
 
+#if demonize
     pid = fork();
     if (pid < 0) {
         exit(1);
@@ -500,6 +585,7 @@ int main(int argc, char **argv) {
     umask(0);
 
     kill_previous_instance();
+#endif
 
     lock_fd = create_pid_file();
     if (!lock_fd) {
@@ -507,8 +593,11 @@ int main(int argc, char **argv) {
     }
 
     signal(SIGTERM, signal_handler);
+#if ! demonaize
+    signal(SIGINT, signal_handler);
+#endif
 
-#if 1
+#if demonize
     close(STDIN_FILENO);
     close(STDOUT_FILENO);
     close(STDERR_FILENO);
